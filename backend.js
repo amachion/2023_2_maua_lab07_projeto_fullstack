48b8ff5ffced7f862d7b6cc4fe199f0b6f165d20
Andreia <andreia.machion@saojudas.br>
2023-11-16 08:59:47 -0300

    aula 16/11 até código 2.21.8

--- backend.js
+++ backend.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -20,7 +21,7 @@ usuarioSchema.plugin(uniqueValidator);
 const Usuario = mongoose.model("Usuario", usuarioSchema);
 
 async function conectarMongoDB () {
-    await mongoose.connect(`mongodb+srv://usuario:senha@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority`)
+    await mongoose.connect(`mongodb+srv://pro_mac:mongo1234@cluster0.skf8n.mongodb.net/?retryWrites=true&w=majority`)
 }
 
 //ponto de acesso para requisições get /oi
@@ -45,18 +46,41 @@ app.post ('/filmes', async (req, res) => {
     res.json(filmes);
 });
 app.post ('/signup', async (req, res) => {
+    try {
+        const login = req.body.login;
+        const senha = req.body.senha;
+        const criptografada = await bcrypt.hash(senha, 10);
+        const usuario = new Usuario({
+            login: login, 
+            senha: criptografada
+        });
+        const respostaDoMongo = await usuario.save();
+        console.log (respostaDoMongo);
+        res.status(201).end();
+    }
+    catch (e) {
+        console.log(e);
+        res.status(409).end();
+    }
+})
+app.post('/login', async (req, res) => {
     const login = req.body.login;
     const senha = req.body.senha;
-    const criptografada = await bcrypt.hash(senha, 10);
-    const usuario = new Usuario({
-        login: login, 
-        senha: criptografada
-    });
-    const respostaDoMongo = await usuario.save();
-    console.log (respostaDoMongo);
-    res.end();
+    const user = await Usuario.findOne({login: login});
+    if (!user) {
+        res.status(401).json({mensagem: "usuário não cadastrado"});
+    }
+    const senhaValida = await bcrypt.compare(senha, user.senha);
+    if (!senhaValida) {
+        res.status(401).json({mensagem: "senha inválida"});
+    }
+    const token = jwt.sign(
+        {login: login},
+        "minha-chave",
+        {expiresIn: "1h"}
+    )
+    res.status(200).json({token: token});
 })
-
 
 
 app.listen(3000, () => {
